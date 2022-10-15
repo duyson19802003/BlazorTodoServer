1823ade4f71e24a3e59d63958b7d7735919c0c6c
DuySon <duyson19802003@gmail.com>
2022-10-15 17:20:27 +0700

    Add project files.

---
+++ Pages/FetchData.razor.cs
@@ -0,0 +1,40 @@
+﻿using BlazorTodoServer.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTodoServer.Pages
+{
+    /// <summary>
+    /// first way for code behind: using partial class
+    /// </summary>
+    //public partial class FetchData
+    //{
+        
+    //private WeatherForecast[] forecasts;
+
+    //    //using dependency injection
+    //    [Inject] WeatherForecastService ForecastService { get; set; }
+
+    //    protected override async Task OnInitializedAsync()
+    //    {
+    //        forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+    //    }
+    //}
+
+    
+    public class FetchDataModel:ComponentBase
+    {
+        protected WeatherForecast[] forecasts;
+        //using dependency injection
+        [Inject] WeatherForecastService ForecastService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+
+}
